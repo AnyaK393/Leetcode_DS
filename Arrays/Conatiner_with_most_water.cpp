b08281ef7b1b61249d46eacabc964344be77b228
Ananya <ananyak0305@gmail.com>
2026-06-04 12:28:02 +0530

    2 array question added

---
+++ Arrays/Conatiner_with_most_water.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left=0;
+        int right= height.size()-1;
+        int maxArea=0;
+
+        while(left<right){
+            int width= right-left;
+            int currentArea= width *min(height[left], height[right]);
+
+            maxArea= max(currentArea, maxArea);
+
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+
+        }
+        return maxArea;
+    }
+};
