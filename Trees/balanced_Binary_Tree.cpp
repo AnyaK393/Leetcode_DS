3164403e0d0cfd05cac9bda1e507e9818751ebb4
Ananya <ananyak0305@gmail.com>
2026-06-11 12:30:07 +0530

    4 binary tree questions added

---
+++ Trees/balanced_Binary_Tree.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() {
+        val = 0;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    TreeNode(int x) {
+        val = x;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) {
+        val = x;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+class Solution {
+public:
+
+    int height(TreeNode* root)
+    {
+        if(root == nullptr)
+        {
+            return 0;
+        }
+
+        int left = height(root->left);
+
+        if(left == -1)
+        {
+            return -1;
+        }
+
+        int right = height(root->right);
+
+        if(right == -1)
+        {
+            return -1;
+        }
+
+        if(abs(left - right) > 1)
+        {
+            return -1;
+        }
+
+        return 1 + max(left, right);
+    }
+
+    bool isBalanced(TreeNode* root)
+    {
+        return height(root) != -1;
+    }
+};
