7380e42c66848362180febb5e6306f34a8449d3a
Ananya <ananyak0305@gmail.com>
2026-06-23 12:48:35 +0530

    1 array question added

---
+++ Arrays/Find_Pivot_Index.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+       int totalSum=0;
+       for(int i=0;i<nums.size();i++){
+            totalSum+= nums[i];
+       } 
+
+       int leftSum=0;
+       for(int i=0;i<nums.size();i++){
+        int rightSum= totalSum-leftSum-nums[i];
+            if(rightSum==leftSum){
+                return i;
+            }
+            leftSum+=nums[i];
+       }
+       return -1;
+    }
+};
