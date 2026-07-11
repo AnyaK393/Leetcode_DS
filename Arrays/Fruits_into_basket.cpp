ef19e6d49e738520a25e45e6ee79122d26dcf45d
Ananya <ananyak0305@gmail.com>
2026-07-11 12:16:39 +0530

    2 variable sliding window questions added

---
+++ Arrays/Fruits_into_basket.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+       int left=0;
+       int answer=0;
+       unordered_map<int, int> freq;
+
+       for(int right=0;right<fruits.size();right++){
+            freq[fruits[right]]++;
+
+            while(freq.size()>2){
+                freq[fruits[left]]--;
+
+                if(freq[fruits[left]]==0)
+                    freq.erase(fruits[left]);
+                left++;
+            }
+            answer= max(answer, right-left+1);
+       } 
+       return answer;
+    }
+};
