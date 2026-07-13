4abf6d682fd3f4d1869951ea97c93926c72f61f3
Ananya <ananyak0305@gmail.com>
2026-07-13 20:11:31 +0530

    1 variable sliding window question added

---
+++ Arrays/Longest_Subarray_After_deleting_1_element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left=0;
+        int answer=0;
+        int zeroCount=0;
+        for(int right=0;right<nums.size();right++){
+            if(nums[right]==0){
+                zeroCount++;
+            }
+            while(zeroCount>1){
+                if(nums[left]==0){
+                    zeroCount--;
+                }
+                left++;
+            }
+            answer= max(answer, right-left);
+        }
+        return answer;
+    }
+};
